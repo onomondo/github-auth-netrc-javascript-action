c6b5c2e9a7ca38ec9599d129d8aeeb2b4294b51f
Kasper Jacobsen <k@mackwerk.dk>
2019-09-17 20:18:07 +0200

    Try something different

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const core = require('@actions/core')
 
 try {
   const authToken = core.getInput('github-auth-token')
-  const netRcPath = path.join(process.env.HOME, '.netrc')
-  const netRcContents = `machine github.com login ${authToken} password x-oauth-basic`
-  fs.writeFileSync(netRcPath, netRcContents)
-  console.log(`Wrote ${netRcPath}`)
-  const t = fs.readFileSync(path.join(process.env.HOME, '.ssh', 'id_rsa.pub'))
-  console.log('t', t)
+  // const netRcPath = path.join(process.env.HOME, '.netrc')
+  // const netRcContents = `machine github.com login ${authToken} password x-oauth-basic`
+  // fs.writeFileSync(netRcPath, netRcContents)
+  // console.log(`Wrote ${netRcPath}`)
+  const dir = path.join(process.env.HOME, '.ssh')
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  fs.writeFileSync(path.join(dir, 'id_rsa.pub'), authToken)
+  console.log('wrote ssh key')
+  console.log('contents', fs.readFileSync(path.join(dir, 'id_rsa.pub')))
 } catch (error) {
   core.setFailed(error.message)
 }
