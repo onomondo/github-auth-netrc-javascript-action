8248a7ead3ad71432198c89ac8b52768383dea9e
Kasper Jacobsen <k@mackwerk.dk>
2019-09-17 20:09:59 +0200

    Test

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ try {
   const netRcContents = `machine github.com login ${authToken} password x-oauth-basic`
   fs.writeFileSync(netRcPath, netRcContents)
   console.log(`Wrote ${netRcPath}`)
+  const t = fs.readFileSync(path.join(process.env.HOME, '.ssh', 'id_rsa.pub'))
+  console.log('t', t)
 } catch (error) {
   core.setFailed(error.message)
 }
