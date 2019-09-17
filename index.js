4bcdff3b306551ed7e67e421a52687071532d4cc
Kasper Jacobsen <k@mackwerk.dk>
2019-09-17 19:46:36 +0200

    This should be enough for the action, but it doesnt work :(

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const core = require('@actions/core')
-const github = require('@actions/github')
 
 try {
-  console.log('process.env', process.env)
   const authToken = core.getInput('github-auth-token')
   const netRcPath = path.join(process.env.HOME, '.netrc')
-  const netRcContents = `machine github.com login ${process.env.GITHUB_TOKEN} password x-oauth-basic`
+  const netRcContents = `machine github.com login ${authToken} password x-oauth-basic`
   fs.writeFileSync(netRcPath, netRcContents)
-  const x = fs.readFileSync(netRcPath)
-  console.log('x', x.toString())
-
-  const curlRcPath = path.join(process.env.HOME, '.netrc')
-  const curlRcContents = `--net-rc`
-  fs.writeFileSync(curlRcPath, curlRcContents)
+  core.info(`Wrote ${netRcPath}`)
 } catch (error) {
   core.setFailed(error.message)
 }
