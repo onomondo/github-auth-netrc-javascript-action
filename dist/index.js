47a9989b6e6e85cd65badc808a9ceb7da9a7c9d4
Kasper Jacobsen <k@mackwerk.dk>
2019-09-17 17:12:31 +0200

    Clean

--- dist/index.js
+++ dist/index.js
@@ -516,17 +516,6 @@ try {
   const filePath = path.join(process.env.HOME, '.netrc')
   const fileContents = `machine github.com login ${authToken} password x-oauth-basic`
   fs.writeFileSync(filePath)
-  console.log(`Wrote GITHUB_AUTH_TOKEN to ${filePath}`)
-  const result = fs.readFileSync(filePath).toString()
-  console.log('result:', result)
-  const dirResult = fs.readdirSync(process.env.HOME)
-  console.log('dir result:', dirResult)
-
-  const curlRcPath = path.join(process.env.HOME, '.curlrc')
-  fs.writeFileSync(curlRcPath, '--netrc-optional')
-
-  const dirResult2 = fs.readdirSync(process.env.HOME)
-  console.log('dir result 2:', dirResult2)
 } catch (error) {
   core.setFailed(error.message)
 }
