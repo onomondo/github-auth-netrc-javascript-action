3eab6697c7cedb1d29e54cfc10f826441a940185
Kasper Jacobsen <k@mackwerk.dk>
2019-10-28 11:39:47 +0100

    It works

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Github Auth netrc action
+# Github Auth action
 
 This action uses a GitHub OAuth2 token to resolve private repository urls without putting secrets in local files.
 
-In all aspects this action is heavily inspired by [timshadel/heroku-buildpack-github-netrc](https://github.com/timshadel/heroku-buildpack-github-netrc)
+In all aspects this action is heavily inspired by [timshadel/heroku-buildpack-github-netrc](https://github.com/timshadel/heroku-buildpack-github-netrc) and [The Vanilla DevOps Git Credentials & Private Packages Cheatsheet](https://coolaj86.com/articles/vanilla-devops-git-credentials-cheatsheet/).
 
 ## Requirements
 
@@ -11,7 +11,7 @@ Expects a `secret` available in the repo called `GITHUB_AUTH_TOKEN`
 ## Example usage
 
 ```yml
-uses: onomondo/github-auth-netrc-javascript-action
+uses: onomondo/github-auth-javascript-action
 with:
   github-auth-token: ${{ secrets.GITHUB_AUTH_TOKEN }}
 ```
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+// https://coolaj86.com/articles/vanilla-devops-git-credentials-cheatsheet/
 const fs = require('fs')
 const path = require('path')
 const core = require('@actions/core')
 
-const createGitConfig = (contents) => `
-${contents}
+const gitConfigTemplate = `
 [url "https://api.github.com/"]
     insteadOf = "https://github.com/"
 [url "https://ssh@github.com/"]
@@ -14,20 +14,15 @@ ${contents}
 
 try {
   const authToken = core.getInput('github-auth-token')
-  // const netRcPath = path.join(process.env.HOME, '.netrc')
-  // const netRcContents = `machine github.com login ${authToken} password x-oauth-basic`
-  // fs.writeFileSync(netRcPath, netRcContents)
   const gitConfigPath = path.join(process.env.HOME, '.gitconfig')
-  // const gitConfigContents = fs.readFileSync(gitConfigPath).toString()
-  // console.log('gitConfigContents', gitConfigContents)
-  fs.writeFileSync(gitConfigPath, createGitConfig(''))
-  console.log(`Wrote ${gitConfigPath}`)
+  fs.writeFileSync(gitConfigPath, gitConfigTemplate)
 
   const askPassPath = `${process.env.HOME}/.git-askpass`
   fs.writeFileSync(askPassPath, `echo ${authToken}`)
   fs.chmodSync(askPassPath, '0700')
   core.exportVariable('GIT_ASKPASS', askPassPath);
-  console.log(`Wrote ${askPassPath}`)
+
+  console.log(`Wrote ${gitConfigPath} and ${askPassPath}`)
 } catch (error) {
   core.setFailed(error.message)
 }
